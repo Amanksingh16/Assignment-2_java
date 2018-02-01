c75269c87abe3e3eaff54d81d013b20b08d9d686
Amanksingh16 <amansingh16597@gmail.com>
2018-01-31 19:23:11 -0800

    A Stairs like Pattern

---
+++ Class2_Pattern_AmanSingh.java
@@ -0,0 +1,22 @@
+package class2;
+
+public class Class2 
+{
+	public static void main(String[] args) 
+	{
+		int i=1;
+		while(i<=4)
+		{
+			int j=1;
+			while(j<=i)
+			{
+				System.out.print(1);
+				j++;
+			}
+			System.out.println();
+			i++;
+		}
+
+	}
+
+}
