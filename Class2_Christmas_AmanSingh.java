4af39096ef7a86c17d749b4541d91bcc428ba0a6
Amanksingh16 <amansingh16597@gmail.com>
2018-01-31 19:23:42 -0800

    Small Christmas tree printed

---
+++ Class2_Christmas_AmanSingh.java
@@ -0,0 +1,20 @@
+package class2;
+
+public class Class2_Christmas_AmanSingh 
+{
+	public static void main(String[] args)
+{
+	for(int i=1;i<8;i++)
+	{
+		for(int j=1;j<8-i;j++)
+		{
+			System.out.print("  ");
+		}			
+		for(int k=1;k<=(2*i)-1;k++)
+		{
+			System.out.print("* ");
+		}
+		System.out.println();
+	}
+}
+}
