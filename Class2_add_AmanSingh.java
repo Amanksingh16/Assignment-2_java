0dc2b35900b71c6d2b4f2542593450bb5d295077
Amanksingh16 <amansingh16597@gmail.com>
2018-01-31 19:21:54 -0800

    Two numbers added

---
+++ Class2_add_AmanSingh.java
@@ -0,0 +1,10 @@
+package class2;
+
+public class Class2_add_AmanSingh {
+public static void main(String[] args)
+{
+	int a=12,b=43;
+    int sum=a+b;
+    System.out.println("The sum of two numbers are "+sum);
+}
+}
