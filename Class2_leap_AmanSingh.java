9b96a8ae6382591784a8792403ea136d0390f1df
Amanksingh16 <amansingh16597@gmail.com>
2018-01-31 19:25:23 -0800

    Whether a Year is a Leap Year

---
+++ Class2_leap_AmanSingh.java
@@ -0,0 +1,16 @@
+package class2;
+
+public class Class2_leap_AmanSingh {
+public static void main(String[] args)
+{
+	int year=2016;
+	if(year%4==0)
+	{
+		System.out.println("It is a leap year");
+	}
+	else
+	{
+		System.out.println("It is not a leap year");
+	}
+}
+}
