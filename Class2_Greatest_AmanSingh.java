0fe0895a662e44677ccf10d1996db434d5e8d916
Amanksingh16 <amansingh16597@gmail.com>
2018-01-31 19:24:25 -0800

    The Greatest of three numbers is found

---
+++ Class2_Greatest_AmanSingh.java
@@ -0,0 +1,24 @@
+package class2;
+
+public class Class2_Greatest_AmanSingh
+{
+public static void main(String[] args)
+{
+	int a=23,b=11,c=45;
+	if ((a>b) && (a>c))
+	{
+		System.out.println(" a is a greatest ");
+	}
+	else
+		if ((b>a) && (b>c))
+		{
+			System.out.println(" b is a greatest ");
+		}
+		else
+		{
+			System.out.println(" c is a greatest ");
+        }	
+	
+}
+
+}
